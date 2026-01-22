dbc9d2f339849a30e61b381032c76f85d78b18d1
Dapples <lamzac26@gmail.com>
2026-01-23 00:13:26 +1100

    extended portability to windows

--- main.cpp
+++ main.cpp
@@ -3,12 +3,16 @@
 #include "src/gb.h"
 
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: ./GravelBoy path/to/rom\n";
+        exit(1);
+    }
     gb gb;
     // 02-interrupts instr_timing interrupt_time dmg-acid2 cgb-acid2.gbc cpu_instrs
     // 01-read_timing  02-write_timing 03-modify_timing
     // mbctest MBC3_Test.gbc rtc3test
-    gb.run("rom/zelda.gbc");
+    gb.run((const char*)argv[1]);
 
 
 
